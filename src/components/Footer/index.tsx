d47ec55d51a989eda6b487e6356de7ca069ba604
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2021-10-26 18:50:43 +0200

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import {
+    Flex,
+    useColorModeValue
+} from '@chakra-ui/react';
+
+const Footer = () => (
+    <Flex position="fixed" left="0" bottom="0" width="100%" alignItems="flex-start" justifyContent="space-around" bg={useColorModeValue('gray.100', 'gray.900')}>
+        
+        <Flex direction="column" p="4" rounded="6">
+            <h2><b>Contributors:</b></h2>
+            <p>Erlend Sorknes</p>
+            <p>Mattis Czternasty Hembre</p>
+            <p>Henrik Bjørnstad Skog</p>
+        </Flex>
+        <Flex direction="column" p="4" rounded="6">
+            <h2><b>Contact us:</b></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe officia animi iusto!</p>
+        </Flex>
+
+    </Flex>
+)
+
+export default Footer
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Homepage from './pages/homepage/homepage';
 import reportWebVitals from './reportWebVitals';
-
+import Footer from './components/Footer'
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react"
@@ -12,6 +12,8 @@ ReactDOM.render(
   <ChakraProvider>
   <React.StrictMode>
     <Homepage />
+
+    <Footer></Footer>
   </React.StrictMode>
   </ChakraProvider>,
   document.getElementById('root')
