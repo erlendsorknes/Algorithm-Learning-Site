f5508f47feae164220378a217382823a529f95f6
Erlend0703 <Sorknerl@gmail.com>
2021-10-26 17:44:17 +0200

    organized structure of project

---
+++ src/pages/homepage/homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../../logo.svg';
+import '../../../App.css';
+import Header from "../../header/header";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+     <Header />
+    </div>
+  );
+}
+
+
+export default Homepage;
