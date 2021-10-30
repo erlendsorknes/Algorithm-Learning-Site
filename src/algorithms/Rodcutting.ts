9cb88300da6ec3a9e39b7f654fb38cef7f50e2a8
Henrik <henrikskog01@gmail.com>
2021-10-30 21:46:42 +0200

    add rodcutting algorithm class

---
+++ src/algorithms/Rodcutting.ts
@@ -0,0 +1,14 @@
+class Rodcutting {
+    values: Number[] = [3, 5, 8, 4, 10, 12]
+
+    constructor(values: Number[]) {
+        this.values = values;
+    }
+
+    solve() {
+
+    }
+
+}
+
+export default Rodcutting;
