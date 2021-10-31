19127c8568d536731e25667c6fad201c3fb1d5f6
Henrik <henrikskog01@gmail.com>
2021-10-31 20:45:22 +0100

    add wip on algo class

--- src/algorithms/Rodcutting.ts
+++ src/algorithms/Rodcutting.ts
@@ -1,14 +1,29 @@
 class Rodcutting {
-    values: Number[] = [3, 5, 8, 4, 10, 12]
+    values: number[] = [3, 5, 8, 4, 10, 12]
+    memory: number[]
+    length: number;
 
-    constructor(values: Number[]) {
+    constructor(values: number[], length: number) {
         this.values = values;
+        this.memory = new Array(length).map(_ => 0);
+        this.length = length;
+
+        this.memory[0] = values[0]
     }
 
-    solve() {
+    solve(len: number): number {
+        if (len === 1) {
+            return this.values[0]
+        } else if (this.memory[len - 1] !== 0) {
+            return this.memory[len - 1]
 
+        }
+        for (let i = 1; i < len-1; i++) {
+            const cost = this.values[i-1] + this.solve(len-1);
+        }
+
+        return 0
     }
-
 }
 
 export default Rodcutting;
