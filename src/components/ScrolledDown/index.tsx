d639be7a33455a396f4d97fa808f75000cb1dd82
Erlend <sorknerl@gmail.com>
2021-11-05 11:16:36 +0100

    added new picture for frontpage

--- src/components/ScrolledDown/index.tsx
+++ src/components/ScrolledDown/index.tsx
@@ -8,35 +8,37 @@ const ScrolledDown = () => {
             textAlign={'center'}
             mt="70px"
             fontSize="40px"
-            color="#16161D"
             font="futura-pt"
             mb="50px"
             >
             <Heading
-            color="white">
-                Some of our work
+            fontFamily="Consolas">
+                Algorithms
             </Heading>
             </Container>
             <Flex direction="row" justifyContent="space-around" mt="30px">
                 <Box mb="100px">
-                    <Image src="https://i.ytimg.com/vi/UMlFFFXj9Bw/maxresdefault.jpg"
-                    boxSize="400px"
+                    <Image src="https://images.unsplash.com/photo-1520697517317-6767553cc51a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+                    boxSize="300px"
                     />
-                    <Heading as="h3" size="lg" mt="20px" ml="20px">
+                    <Heading as="h3" size="lg" mt="20px" ml="5px" fontFamily="Consolas">
                         The rot cutting problem
                     </Heading>
                     <Text fontSize="lg">
                         Solving the classic "Rod cutting problem" with algorithms
                     </Text>
-
                 </Box>
                 <Box>
                     <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/512px-Bubblesort-edited-color.svg.png"
-                    boxSize="400px"
+                    boxSize="300px"
                     />
-                    <Heading as="h3" size="lg" mt="20px" ml="120px">
+                    <Heading as="h3" size="lg" mt="20px" ml="60px" fontFamily="Consolas">
                         Bubble Sort
                     </Heading>
+                    <Text fontSize="lg" ml="40px">
+                        Learn how bubble sort works
+
+                    </Text>
                 </Box>
             </Flex>
         </Box>
