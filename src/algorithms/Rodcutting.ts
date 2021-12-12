61ab50c025fc1c40b3e53775f345aa49d4c89036
Henrik Skog <henrikskog01@gmail.com>
2021-12-12 14:20:07 +0100

    format

--- src/algorithms/Rodcutting.ts
+++ src/algorithms/Rodcutting.ts
@@ -1,51 +1,76 @@
 // @ts-ignore
 class Rodcutting {
-    values: number[]
-    memory: number[]
-    length: number
+  values: number[];
+  memory: number[] = [];
+  length: number;
 
-    constructor(length: number, values: number[] = [3, 5, 8, 4, 10, 12]) {
-        this.values = values;
-        this.memory = new Array(length).map(_ => 0);
-        this.length = length;
+  constructor(length: number, values: number[] = [2, 5, 8, 4, 10, 12]) {
+    this.values = values;
+    this.length = length;
 
-        this.memory[0] = values[0]
+    for (let i = 0; i < length; i++) {
+      this.memory[i] = -1;
+    }
+  }
+
+  solve() {
+    return this.internal_solve(this.length);
+  }
+
+  solve_brute() {
+    return this.internal_solve(this.length);
+  }
+
+  internal_solve(len: number): number {
+    if (len === 0) {
+      return 0;
+    } else if (this.memory[len - 1] !== -1) {
+      return this.memory[len - 1];
     }
 
-    solve() {
-        return this.internal_solve(this.length);
+    let max = 0;
+
+    for (let i = 0; i < len; i++) {
+      let cost: number = this.values[i] + this.internal_solve(len - i - 1);
+
+      if (cost > max) {
+        max = cost;
+      }
     }
 
-    internal_solve(len: number): number {
-        if (len === 0) {
-            return 0
-        }
+    this.memory[len - 1] = max;
 
-        let max = 0;
-
-        for (let i = 0; i < len; i++) {
-            let cost: number;
-            let p1: number = this.memory[i] ? this.memory[i] :
-
-
-            if (this.memory[len-i-1] !== 0) {
-                cost = this.memory[len-i-1]
-            } else {
-                cost = this.values[i] + this.internal_solve(len-i-1)
-                this.memory[len-i-1] = cost;
-            }
-
-            if (cost > max) {
-                max = cost
-            }
-        }
-
-        console.log(max)
-        return max
+    if (len === this.length) {
+      console.log(max);
     }
+
+    return max;
+  }
+
+  internal_solve_brute(len: number) {
+    if (len === 0) {
+      return 0;
+    }
+    let max = 0;
+
+    for (let i = 0; i < len; i++) {
+      let cost: number = this.values[i] + this.internal_solve(len - i - 1);
+
+      if (cost > max) {
+        max = cost;
+      }
+    }
+
+    if (len === this.length) {
+      console.log(max);
+    }
+
+    return max;
+  }
 }
 
-let t = new Rodcutting(4);
-t.solve()
+// 3, 5, 8, 4, 10, 12
+let t = new Rodcutting(6);
+t.solve();
 
 // export default Rodcutting;
